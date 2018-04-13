a59c53a90ce051b4894ed6924cced372e56990ba
bglaub <blakeglaub@gmail.com>
2018-04-13 13:26:09 -0500

    add bootstrap-sass, improve webpack logging, and added watch

--- ui/ui.js
+++ ui/ui.js
@@ -8,33 +8,33 @@
     let commands = yargs.argv._;
     let argv = yargs.argv;
 
+    let errHandler = (err, stats) => {
+        if (err) {
+            console.error(err.stack || err);
+            console.log('');
+
+            if (err.details) {
+              console.error(err.details);
+              console.log('');
+            }
+
+            return;
+        }
+
+        console.log(stats.toString());
+        console.log('');
+    };
+
     switch(commands[0]) {
+        case 'watch':
+            let watchConfig = require('webpack-merge')(require('./webpack/config/base.config.js'), require('./webpack/config/dev.config.js'));
+            require('webpack')(watchConfig).watch({ignored: /node_modules/}, errHandler);
+            break;
         case 'build':
             console.log('Building!');
             let modeConfig = require('./webpack/config/' + ((argv.d || argv.development) ? 'dev' : 'prod') + '.config.js');
             let config = require('webpack-merge')(require('./webpack/config/base.config.js'), modeConfig);
-
-            require("webpack")(config).run((err, stats) => {
-                if (err) {
-                    console.error(err.stack || err);
-
-                    if (err.details) {
-                      console.error(err.details);
-                    }
-
-                    return;
-                }
-
-                const info = stats.toJson();
-
-                if (stats.hasErrors()) {
-                    console.error(info.errors);
-                }
-
-                if (stats.hasWarnings()) {
-                    console.warn(info.warnings);
-                }
-            });
+            require('webpack')(config).run(errHandler);
             break;
         default:
 
