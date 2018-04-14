db0d0482a46ace8080841e2f235ef70d449e25f5
bglaub <blakeglaub@gmail.com>
2018-04-14 06:00:34 -0500

    info updates

--- ui/src/states/home/home.controller.js
+++ ui/src/states/home/home.controller.js
@@ -1,67 +1,8 @@
 (function(){
     'use strict';
     module.exports = function(app, angular) {
-        var jQuery = require('jquery');
-        
         return function() {
             this.coverVideoUrl = 'videos/Valentines';
-            
-            this.initialize = function(){
-                scaleVideoContainer();
-
-                initBannerVideoSize('.video-container .poster img');
-                initBannerVideoSize('.video-container .filter');
-                initBannerVideoSize('.video-container video');
-            };
-            
-            function scaleVideoContainer() {
-
-                var height = jQuery(window).height() + 5;
-                var unitHeight = parseInt(height) + 'px';
-                jQuery('.homepage-hero-module').css('height',unitHeight);
-
-            }
-
-            function initBannerVideoSize(element){
-
-                jQuery(element).each(function(){
-                    jQuery(this).data('height', jQuery(this).height());
-                    jQuery(this).data('width', jQuery(this).width());
-                });
-
-                scaleBannerVideoSize(element);
-
-            }
-            
-            function scaleBannerVideoSize(element){
-
-                var windowWidth = jQuery(window).width(),
-                windowHeight = jQuery(window).height() + 5,
-                videoWidth,
-                videoHeight;
-
-                // console.log(windowHeight);
-
-                jQuery(element).each(function(){
-                    var videoAspectRatio = jQuery(this).data('height')/jQuery(this).data('width');
-
-                    jQuery(this).width(windowWidth);
-
-                    if(windowWidth < 1000){
-                        videoHeight = windowHeight;
-                        videoWidth = videoHeight / videoAspectRatio;
-                        jQuery(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
-
-                        jQuery(this).width(videoWidth).height(videoHeight);
-                    }
-
-                    jQuery('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
-                });
-            }
-
-            
-            this.initialize();
         };
     };
 })();
